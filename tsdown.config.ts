16f32855f93d889b87934b9e0e90d1487ccae724
Jonathan Netley <Jonathan.Netley@nbcuni.com>
2025-06-30 07:45:28 +0200

    build: cleanup tsdown config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from 'tsdown'
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: ['./src/index.ts'],
-  platform: 'node',
+  entry: ["./src/index.ts"],
+  platform: "node",
   dts: {
     isolatedDeclarations: true,
   },
-  noExternal: ['zod', '@actions/core', '@actions/github'],
-})
+});
